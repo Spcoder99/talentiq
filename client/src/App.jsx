c2d3d2732f22a0dca65b559fac75ccd8192e8c4b
Spcoder99 <ITMET24S0078@itmuniversity1.ac.in>
2026-03-04 18:24:16 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,31 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignIn,
+  SignInButton,
+  SignOutButton,
+  UserButton,
+} from "@clerk/clerk-react";
+
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to the app</h1>
+
+      <SignedOut>
+        <SignInButton mode="modal" >
+          <button className="">Login</button>
+        </SignInButton>
+      </SignedOut>
+
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
+
+      <UserButton />
+    </>
+  );
+}
+
+export default App;
---
+++ server/src/server.js
@@ -0,0 +1,52 @@
+import express from "express";
+import cors from 'cors';
+import path from'path';
+import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
+import {serve} from 'inngest/express'
+import { inngest, functions } from "./lib/inngest.js";
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("ngrok-skip-browser-warning", "true");
+  next();
+});
+
+
+const __dirname=path.resolve();
+
+// Middleware
+app.use(express.json());
+// credentials:true meaning?? => server allows a browser to include cookies on request 
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }))
+
+app.use("/api/inngest", serve({client: inngest, functions}))
+
+app.get("/health", (req, res) => {
+    res.status(200).json({ msg: "api is up and running" });
+})
+
+
+// make our app ready for deployment
+if(ENV.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/dist")));
+
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+    });
+};
+
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(ENV.PORT, () =>
+            console.log("Server is running on port:", ENV.PORT)
+        )
+    } catch (error) {
+        console.error("💥Error starting the server", error);
+    }
+}
+
+startServer()
