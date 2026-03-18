808fb28521e1dd1b7ab50102a3f80aebaea2e496
Spcoder99 <ITMET24S0078@itmuniversity1.ac.in>
2026-03-18 12:41:12 +0530

    new changes added succcessfully round last before

--- client/src/components/AIHintsPanel.jsx
+++ client/src/components/AIHintsPanel.jsx
@@ -46,7 +46,7 @@ Hint 2: ${res?.data?.hints?.[1]?.hintText || ""}
         }
 
         setLoadingHints(false);
-      }, 6000);
+      }, 4200);
 
     } catch (err) {
       console.error("Hints fetch error:", err);
@@ -72,7 +72,7 @@ Hint 2: ${res?.data?.hints?.[1]?.hintText || ""}
 
         setHint(null);
         setLoadingFull(false);
-      }, 10000);
+      }, 8200);
 
     } catch (err) {
       console.error("Full code fetch error:", err);
